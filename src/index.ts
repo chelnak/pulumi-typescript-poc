de47f23c1eb0a48e8aeb0bafa2e6726a1fb518c3
Craig Gumbley <craiggumbley@gmail.com>
2019-11-20 21:21:28 +0000

    Pass extra args to layer resources

--- src/index.ts
+++ src/index.ts
@@ -4,17 +4,28 @@ import * as environment from './environment';
 
 const stack = pulumi.getStack();
 const config = new pulumi.Config('metadata');
-const program = config.get('program');
-const service = config.get('service');
+const program = config.require('program');
+const service = config.require('service');
 const logger = pulumi.log;
 
 logger.info(`Creating management layer for ${stack}`);
 let prefix = `${program}-${stack}-${service}`;
-const m = new management.ManagementLayer('management-resources', { prefix });
+const m = new management.ManagementLayer('management-resources', {
+  prefix,
+  program,
+  service,
+  environment: stack,
+});
 
 const environments: string[] = config.requireObject('environments');
-environments.forEach((x) => {
+environments.forEach((x, i) => {
   prefix = `${program}-${x}-${service}`;
   logger.info(`Creating environment layer for ${x}`);
-  const env = new environment.EnvironmentLayer(`${x}-resources`, { prefix }, { parent: m });
+  const env = new environment.EnvironmentLayer(`${x}-resources`, {
+    prefix,
+    program,
+    service,
+    environment: x,
+    count: i + 1,
+  }, { parent: m });
 });
