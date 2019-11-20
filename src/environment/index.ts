0772b4d63d9d11be3e9169e5fc32867535567c2e
Craig Gumbley <craiggumbley@gmail.com>
2019-11-20 21:23:33 +0000

    Add new resources

--- src/environment/index.ts
+++ src/environment/index.ts
@@ -1,7 +1,10 @@
 import * as pulumi from '@pulumi/pulumi';
 import * as azure from '@pulumi/azure';
-import * as network from '../network';
-import { ILayerArgs } from '../interfaces/management';
+import * as network from '../shared/virtualnetwork';
+import { ILayerArgs } from '../interfaces/args';
+import { IAppServicePlanConfig } from '../interfaces/config';
+import { getAppServicePlanSku } from '../util';
+import * as sql from './sqlserver';
 
 const logger = pulumi.log;
 
@@ -19,12 +22,13 @@ export class EnvironmentLayer extends pulumi.ComponentResource {
     };
 
     super('infrastructure:EnvironmentLayer', name, inputs, opts);
-    const defaultResourceOptions: pulumi.ResourceOptions = { parent: this };
 
     const resourceGroupName = `${layerArgs.prefix}-rg`;
     const resourceGroup = new azure.core.ResourceGroup(resourceGroupName, {
       name: resourceGroupName,
-    }, defaultResourceOptions);
+    }, { parent: this });
+
+    const defaultResourceOptions: pulumi.ResourceOptions = { parent: resourceGroup };
 
     const storageAccoutnName = `${layerArgs.prefix.replace(/-/g, '')}str`;
     const storageAccount = new azure.storage.Account(storageAccoutnName, {
@@ -32,16 +36,74 @@ export class EnvironmentLayer extends pulumi.ComponentResource {
       name: storageAccoutnName,
       accountTier: 'Standard',
       accountReplicationType: 'LRS',
-    }, { parent: resourceGroup });
+    }, defaultResourceOptions);
+
+    // Improve
+    const configStorageAccoutnName = `${layerArgs.program}${layerArgs.environment}configstr`;
+    const configStorageAccount = new azure.storage.Account(configStorageAccoutnName, {
+      resourceGroupName: resourceGroup.name,
+      name: configStorageAccoutnName,
+      accountTier: 'Standard',
+      accountReplicationType: 'LRS',
+    }, defaultResourceOptions);
 
     const vnetName = `${layerArgs.prefix}-vnet`;
     const vnet = new network.VirtualNetwork(vnetName, {
       name: vnetName,
       resourceGroupName: resourceGroup.name,
+      virtualNetowrkPrefix: `10.${layerArgs.count}`,
       gatewaySubnetCount: 1,
       managementSubnetCount: 1,
       internalSubnetCount: 1,
       externalSubnetCount: 1,
-    }, { parent: resourceGroup });
+    }, defaultResourceOptions);
+
+    const appServicePlanConfig: IAppServicePlanConfig[] = new pulumi.Config('environment').requireObject('appserviceplan');
+    appServicePlanConfig.forEach((x) => {
+      const appServicePlanName = `${layerArgs.prefix}${x.name}-asp`;
+      const appServicePlan = new azure.appservice.Plan(appServicePlanName, {
+        name: appServicePlanName,
+        resourceGroupName: resourceGroup.name,
+        sku: getAppServicePlanSku(x.size),
+      }, defaultResourceOptions);
+    });
+
+    const redisCacheName = `${layerArgs.prefix}-rds`;
+    const redisCache = new azure.redis.Cache(redisCacheName, {
+      name: redisCacheName,
+      resourceGroupName: resourceGroup.name,
+      capacity: 1,
+      family: 'C',
+      skuName: 'Standard',
+    }, defaultResourceOptions);
+
+    const cdnProfileName = `${layerArgs.prefix}-cdn`;
+    const cdnProfile = new azure.cdn.Profile(cdnProfileName, {
+      name: cdnProfileName,
+      resourceGroupName: resourceGroup.name,
+      sku: 'Standard_Verizon',
+    }, defaultResourceOptions);
+
+    // investigate properties here: https://github.com/SkillsFundingAgency/das-shared-infrastructure/blob/7df7ae873e1a8393f98cf7d482fb6fc71051a309/templates/environment.template.json#L573
+    const eventHubNamespaceName = `${layerArgs.prefix}-eh`;
+    const eventHubNamespace = new azure.eventhub.Namespace(eventHubNamespaceName, {
+      name: eventHubNamespaceName,
+      resourceGroupName: resourceGroup.name,
+      sku: 'Standard',
+      capacity: 0,
+    }, defaultResourceOptions);
+
+    const serviceBusNamespaceName = `${layerArgs.prefix}-ns`;
+    const serviceBusNamespace = new azure.servicebus.Namespace(serviceBusNamespaceName, {
+      name: serviceBusNamespaceName,
+      resourceGroupName: resourceGroup.name,
+      sku: 'Standard',
+    }, defaultResourceOptions);
+
+    const sqlServer = new sql.SqlServer(`${layerArgs.prefix}-sql`, {
+      prefix: layerArgs.prefix,
+      resourceGroupName: resourceGroup.name,
+      virtualNetworkFirewallRules: vnet.subnetResourceIds,
+    }, defaultResourceOptions);
   }
 }
